7c7932d1973274d99daffbc52e09312ba61a8533
Les Orchard <me@lmorchard.com>
2017-10-21 00:15:42 -0400

    Pumpkin has a web server now what have i done

--- nodemcu/nespad_eyes/nespad_eyes.ino
+++ nodemcu/nespad_eyes/nespad_eyes.ino
@@ -3,10 +3,19 @@
 #include <Adafruit_GFX.h>
 #include <Max72xxPanel.h>
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+const char* ssid = "Subspace";
+const char* password = "p4$$w0rd";
 
 #define POT_PIN 0
 
 #define DEBUG 1
+
+#define SERVER_PORT 80
+#define SERVER_NAME "pumpkin"
 
 #define INTENSITY 0
 #define INTENSITY_MIN 1
@@ -137,35 +146,37 @@ Max72xxPanel matrix = Max72xxPanel(12, 6, 1);
 // put your own strobe/clock/data pin numbers here -- see the pinout in readme.txt
 NESpad nintendo = NESpad(5,4,16);
 
+// Create an instance of the server
+// specify the port to listen on as an argument
+ESP8266WebServer server ( SERVER_PORT );
+
 int i, current, target, pot, pad, idle_pad;
+byte display_intensity;
 unsigned long t_now, t_last;
 long td_eyes, td_wifi, idle_time, idle_move_delay;
 
 #define NUM_TIMERS 2
 #define TIMER_EYES 0
-#define TIMER_WIFI 1
+#define TIMER_SERVER 1
 const PROGMEM long timers_max[] = { 
   25, 
-  30000 
+  100 
 };
 
 long timers[NUM_TIMERS];
 
 void setup() {
   #ifdef DEBUG
-    Serial.begin(9600);
+    Serial.begin(115200);
     while (!Serial) { }
     Serial.println("STARTING UP");
   #endif
 
+  setupWifiServer();  
   resetTimers();
   prerenderPositions();
   resetDisplay();
   updateDisplay();
-  
-  // Set WiFi to station mode and disconnect from an AP if it was previously connected
-  // WiFi.mode(WIFI_STA);
-  // WiFi.disconnect();
 }
 
 void resetTimers() {
@@ -179,6 +190,8 @@ void resetTimers() {
 }
 
 void loop() {
+  server.handleClient();
+
   unsigned long t_delta;
   t_now = millis();
   t_delta = t_now - t_last;
@@ -190,44 +203,144 @@ void loop() {
       long elapsed = timers_max[i] - timers[i];
       switch (i) {
         case TIMER_EYES: loopEyes(elapsed); break;
+        case TIMER_SERVER: loopServer(elapsed); break;
       }
       timers[i] = timers_max[i];
     }
   }
 }
 
-void loopWifi(unsigned long t_delta) {
-  Serial.println("scan start");
-
-  // WiFi.scanNetworks will return the number of networks found
-  int n = WiFi.scanNetworks();
-  Serial.println("scan done");
-  if (n == 0)
-    Serial.println("no networks found");
-  else
-  {
-    Serial.print(n);
-    Serial.println(" networks found");
-    for (int i = 0; i < n; ++i)
-    {
-      // Print SSID and RSSI for each network found
-      Serial.print(i + 1);
-      Serial.print(": ");
-      Serial.print(WiFi.SSID(i));
-      Serial.print(" (");
-      Serial.print(WiFi.RSSI(i));
-      Serial.print(")");
-      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE)?" ":"*");
-      delay(10);
-    }
+void setupWifiServer() {
+  // Connect to WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
   Serial.println("");
+  Serial.println("WiFi connected");
+  
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+  
+  if ( MDNS.begin ( SERVER_NAME ) ) {
+    Serial.println ( "MDNS responder started" );
+  }
+
+  server.on ( "/", handleRoot );
+  server.on ( "/up", handleUp );
+  server.on ( "/down", handleDown );
+  server.on ( "/test.svg", drawGraph );
+  server.on ( "/inline", []() {
+    server.send ( 200, "text/plain", "this works as well" );
+  } );
+  server.onNotFound ( handleNotFound );
+  server.begin();
+  Serial.println ( "HTTP server started" );
+}
+
+void handleUp () {
+  char temp[400];
+  if (display_intensity < INTENSITY_MAX) {
+    setDisplayIntensity(display_intensity + 1);
+  }
+  snprintf(temp, 400, "<html><body>Intensity = %02d (up)</body></html>", display_intensity);
+  server.send ( 200, "text/html", temp );
+}
+
+void handleDown () {  
+  char temp[400];
+  if (display_intensity > INTENSITY_MIN) {
+    setDisplayIntensity(display_intensity - 1);
+  }
+  snprintf(temp, 400, "<html><body>Intensity = %02d (down)</body></html>", display_intensity);
+  server.send ( 200, "text/html", temp );
+}
+
+void handleRoot() {
+  char temp[800];
+  int sec = millis() / 1000;
+  int min = sec / 60;
+  int hr = min / 60;
+
+  snprintf ( temp, 800,
+
+"<html>\
+  <head>\
+    <title>ESP8266 Demo</title>\
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\
+    <style>\
+      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
+    </style>\
+  </head>\
+  <body>\
+    <h1>Hello from ESP8266!</h1>\
+    <p>Uptime: %02d:%02d:%02d</p>\
+    <img src=\"/test.svg\" />\
+    <br>\
+    <h2><a target=\"output\" href=\"/down\">DOWN</a></h2>\
+    <h2><a target=\"output\" href=\"/up\">UP</a></h2>\
+    <br>\
+    <iframe name=\"output\" src=\"/test.svg\"></iframe>\
+  </body>\
+</html>",
+
+    hr, min % 60, sec % 60
+  );
+  server.send ( 200, "text/html", temp );
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+  for ( uint8_t i = 0; i < server.args(); i++ ) {
+    message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
+  }
+
+  server.send ( 404, "text/plain", message );
+}
+
+void drawGraph() {
+  String out = "";
+  char temp[100];
+  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
+  out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
+  out += "<g stroke=\"black\">\n";
+  int y = rand() % 130;
+  for (int x = 10; x < 390; x+= 10) {
+    int y2 = rand() % 130;
+    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
+    out += temp;
+    y = y2;
+  }
+  out += "</g>\n</svg>\n";
+
+  server.send ( 200, "image/svg+xml", out);
+}
+
+void loopServer(unsigned long t_delta) {
 }
 
 void loopEyes(unsigned long t_delta) {
   // Adjust intensity based on the potentiometer position
-  pot = map(analogRead(POT_PIN), 0, 1023, 0, 15);
-  setDisplayIntensity(pot);
+  // pot = map(analogRead(POT_PIN), 0, 1023, 0, 15);
+  // setDisplayIntensity(pot);
 
   // Grab gamepad state
   pad = nintendo.buttons();
@@ -323,8 +436,8 @@ void renderEyelid(int idx, int y) {
 }
 
 // Ensure the displays are on, intense, and cleared.
-void resetDisplay() {  
-  matrix.setIntensity(1); 
+void resetDisplay() {
+  setDisplayIntensity(1);
   matrix.setRotation(0, 3);
   matrix.setRotation(1, 3);
   matrix.setRotation(2, 3);
@@ -374,7 +487,8 @@ void prerenderPositions() {
 
 // Set display intensity
 void setDisplayIntensity(int intensity) {
-  matrix.setIntensity(2);
+  display_intensity = intensity;
+  matrix.setIntensity(display_intensity); 
 }
 
 // Set the LED contents from the display buffers
